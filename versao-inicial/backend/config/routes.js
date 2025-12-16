771dcb82c3685341ce1fbdbe128a89fad8e5c9ed
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-16 09:55:48 -0400

    Direcionando rotas das autenticações.

--- versao-inicial/backend/config/routes.js
+++ versao-inicial/backend/config/routes.js
@@ -1,4 +1,8 @@
 module.exports = app => {
+    app.post('/signup', app.api.user.save)
+    app.post('/signin', app.api.auth.signin)
+    app.post('/validateToken', app.api.auth.validateToken)
+
     app.route('/users')
         .post(app.api.user.save)
         .get(app.api.user.get)
@@ -6,7 +10,6 @@ module.exports = app => {
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getById)
-
 
     app.route('/categories')
         .get(app.api.category.get)
