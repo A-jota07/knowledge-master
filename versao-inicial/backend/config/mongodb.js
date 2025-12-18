8fe0824fcadb0e244f99c90ea594d7284b782a9e
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-18 09:51:09 -0400

    Atualizando as configurações de urls

--- versao-inicial/backend/config/mongodb.js
+++ versao-inicial/backend/config/mongodb.js
@@ -1,5 +1,8 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERROR! Não foi possivel se conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
