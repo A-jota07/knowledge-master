f13b22f5fdd3a5513f8aecf7790468d3e6aabfab
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-19 09:27:25 -0400

    Criando Content, footer, Header e Menu do site.

---
+++ versao-inicial/frontend/src/components/template/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.header {
+  grid-area: header;
+  background: linear-gradient(to right, #9cecfb, #65c7f7, #0052d4);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
