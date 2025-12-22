5e9af9fd99c0fac0d78f72ff202f1ef455de5923
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-22 17:50:42 -0400

    Adicionando os titulos e subtitulos da pagina Admin

---
+++ versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="admin-pages">
+    <Pagetitle
+      icon="fa fa-cogs"
+      main="Administração do Sistema"
+      sub="Cadastros & Cia"
+    />
+  </div>
+</template>
+
+<script>
+import PageTitle from "../template/PageTitle";
+
+export default {
+  name: "AdminPages",
+  components: { PageTitle },
+};
+</script>
+
+<style>
+</style>
