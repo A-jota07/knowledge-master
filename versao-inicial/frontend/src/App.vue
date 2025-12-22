f884fcd283997b9aebeb1897b339a7519ec8ba4a
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-22 13:27:56 -0400

    Adicionando a função de esconder o menu e criando css da função

--- versao-inicial/frontend/src/App.vue
+++ versao-inicial/frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="{ 'hide-menu': !isMenuVisible }">
     <Header title="Junior - Base de postagens" />
     <Menu />
     <Content />
@@ -8,6 +8,7 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import Header from "@/components/template/Header.vue";
 import Menu from "@/components/template/Menu.vue";
 import Content from "@/components/template/Content.vue";
@@ -16,6 +17,7 @@ import Footer from "@/components/template/Footer.vue";
 export default {
   name: "App",
   components: { Header, Menu, Content, Footer },
+  computed: mapState(["isMenuVisible"]),
 };
 </script>
 
@@ -40,5 +42,12 @@ body {
     "header header"
     "menu content"
     "menu footer";
+}
+
+#app.hide-menu {
+  grid-template-areas:
+    "header header"
+    "content content"
+    "footer footer";
 }
 </style>
