ca11a866f052bf3876024cb9b040709dbc74ec16
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-26 14:50:49 -0400

    Adicionando paginação nos artigos.

--- versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
+++ versao-inicial/frontend/src/components/admin/ArticleAdmin.vue
@@ -88,6 +88,12 @@
         </b-button>
       </template>
     </b-table>
+    <b-pagination
+      size="md"
+      v-model="page"
+      :total-row="count"
+      :per-page="limit"
+    />
   </div>
 </template>
 
@@ -119,7 +125,7 @@ export default {
   },
   methods: {
     loadArticles() {
-      const url = `${baseApiUrl}/articles`;
+      const url = `${baseApiUrl}/articles?page=${this.page}`;
       axios.get(url).then((res) => {
         this.articles = res.data.data;
         this.count = res.data.count;
@@ -177,6 +183,11 @@ export default {
           return { value: user.id, text: `${user.name} - ${user.email}` };
         });
       });
+    },
+  },
+  watch: {
+    page() {
+      this.loadArticles();
     },
   },
   mounted() {
