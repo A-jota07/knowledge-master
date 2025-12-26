eb4088e10885146368c719ca026e6bfe5267f1d5
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-26 09:48:30 -0400

    Atualizando a config do mongodb para uma versão mais atual.

--- versao-inicial/backend/config/mongodb.js
+++ versao-inicial/backend/config/mongodb.js
@@ -2,7 +2,13 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {
+        console.log('MongoDB conectado com sucesso')
+    })
     .catch(e => {
         const msg = 'ERROR! Não foi possivel se conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
