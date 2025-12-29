384a141c71e617f3c72b54e4eb45b904343a3c94
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-29 10:13:13 -0400

    Alterando error

--- versao-inicial/backend/config/admin.js
+++ versao-inicial/backend/config/admin.js
@@ -3,7 +3,7 @@ module.exports = middleware => {
         if (req.user.admin) {
             middleware(req, res, next)
         } else {
-            res.status(402).send('usuário não é administrador.')
+            res.status(401).send('usuário não é administrador.')
         }
     }
 }
