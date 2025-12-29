ce2fcaa076f431699accde833acc31f8406d3b0e
Alexandre Cássio de Souza Junior <souzajunioralexandre340@gmail.com>
2025-12-29 09:15:04 -0400

    Removendo o axios e o bearer temporario

--- versao-inicial/frontend/src/main.js
+++ versao-inicial/frontend/src/main.js
@@ -4,7 +4,6 @@ import Vue from 'vue'
 
 import App from './App'
 
-import axios from 'axios'
 import './config/bootstrap'
 import './config/msgs'
 import store from './config/store'
@@ -12,10 +11,6 @@ import router from './config/router'
 
 Vue.config.productionTip = false
 
-if (store.state.user && store.state.user.token) {
-  axios.defaults.headers.common['Authorization'] =
-    `Bearer ${store.state.user.token}`
-}
 
 new Vue({
   store,
